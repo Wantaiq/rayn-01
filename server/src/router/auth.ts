016b6ffbcef4f8445f214a3c7f947324ec781309
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2024-05-31 22:20:23 +0200

    feat: add auth routes

---
+++ server/src/router/auth.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+
+const authRoutes: Router = Router();
+
+authRoutes.post('/register', () => {});
+authRoutes.post('/login', () => {});
+
+export default authRoutes;
