b93c9621adcee1cce41e9ff5ceffd8eeafb4ff48
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2024-06-02 13:00:09 +0200

    feat: use error handler on child router

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -1,7 +1,10 @@
 import Router from 'express';
 import authRoutes from './auth';
+import { errorHandler } from '../middlewares';
 
 const router = Router();
 router.use('/auth', authRoutes);
+
+router.use(errorHandler);
 
 export default router;
