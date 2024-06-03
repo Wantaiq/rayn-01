62115fe6c07702c1060fe239b9a7a86ce5c7bb07
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2024-06-03 21:18:20 +0200

    ci: add tailwindcss with initial colors theme extension

---
+++ client/tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: colors.sky['500'],
+          ...colors.sky,
+        },
+        danger: {
+          DEFAULT: colors.red['500'],
+          ...colors.red,
+        },
+        warning: {
+          DEFAULT: colors.yellow['500'],
+          ...colors.yellow,
+        },
+        success: {
+          DEFAULT: colors.green['500'],
+          ...colors.green,
+        },
+        light: {
+          DEFAULT: colors.slate['50'],
+        },
+        gray: {
+          DEFAULT: colors.gray['400'],
+        },
+      },
+    },
+  },
+  plugins: [],
+};
