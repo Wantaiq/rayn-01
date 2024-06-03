e1d1eab024f57769a69ab02786b2dbcbf3ab9f5e
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2024-06-03 21:23:16 +0200

    feat: add polymorhpic component types

---
+++ client/src/types/polymorphicComponent.ts
@@ -0,0 +1,18 @@
+import {
+  type ComponentPropsWithoutRef,
+  type ElementType,
+} from 'react';
+
+type AsProp<E extends ElementType> = {
+  as?: E;
+};
+
+type PropTypes<E extends ElementType, P> = P &
+  Omit<ComponentPropsWithoutRef<E>, keyof P>;
+
+type PolymorphicComponent<
+  Component extends ElementType,
+  Props = Record<string, unknown>,
+> = AsProp<Component> & PropTypes<Component, Props>;
+
+export { type PolymorphicComponent };
