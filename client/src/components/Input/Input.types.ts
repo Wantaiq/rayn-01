1b3f6f2272fb75a2d101796a96a050ba03ff7c12
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2024-06-10 21:14:57 +0200

    feat: add initial text input component

---
+++ client/src/components/Input/Input.types.ts
@@ -0,0 +1,25 @@
+import type {
+  DetailedHTMLProps,
+  FC,
+  InputHTMLAttributes,
+} from 'react';
+
+export type InputProps = {
+  id?: string;
+  label?: string;
+  inline?: boolean;
+  full: boolean;
+  error?: boolean;
+  type?: 'text' | 'password' | 'email';
+  className?: string;
+} & Omit<
+  DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >,
+  'type'
+>;
+
+type InputComponent = FC<InputProps>;
+
+export default InputComponent;
