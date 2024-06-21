7b6f7ce98c16f8e17217df4cb3a9c0e27f04d156
wantaiq <99611999+Wantaiq@users.noreply.github.com>
2024-06-21 19:55:15 +0200

    ci: add helmet

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,10 @@ import express, { Express } from 'express';
 import router from './router';
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares';
+import helmet from 'helmet';
 
 const app: Express = express();
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
